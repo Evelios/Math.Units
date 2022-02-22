3d25e6e54988fced8d8d1f190f511b1fea993f05
Thomas Waters <twaters117@gmail.com>
2022-02-22 10:02:59 -0500

    Constant helper functions

--- Geometry/Point2D.fs
+++ Geometry/Point2D.fs
@@ -4,11 +4,9 @@ module Geometry.Point2D
 open FSharp.Json
 
 
-(* Builders *)
+// ---- Builders ----
 
 let xy (x: Length<'Unit>) (y: Length<'Unit>) : Point2D<'Unit, 'Coordinates> = { X = x; Y = y }
-
-let meters (x: float) (y: float) : Point2D<Meters, 'Coordinates> = xy (Length.meters x) (Length.meters y)
 
 let rTheta (r: Length<'Unit>) (theta: Angle) : Point2D<'Unit, 'Coordinates> =
     xy (r * Angle.cos theta) (r * Angle.sin theta)
@@ -17,8 +15,18 @@ let polar r theta = rTheta r theta
 
 let origin () : Point2D<'Unit, 'Coordinates> = xy Length.zero Length.zero
 
+// ---- Helper Builder Functions ----
 
-(* Operators *)
+let private fromUnit conversion (x: float) (y: float) : Point2D<Meters, 'Coordinates> = xy (conversion x) (conversion y)
+let meters (x: float) (y: float) : Point2D<Meters, 'Coordinates> = fromUnit Length.meters x y
+let pixels (x: float) (y: float) : Point2D<Meters, 'Coordinates> = fromUnit Length.cssPixels x y
+let millimeters (x: float) (y: float) : Point2D<Meters, 'Coordinates> = fromUnit Length.millimeters x y
+let centimeters (x: float) (y: float) : Point2D<Meters, 'Coordinates> = fromUnit Length.centimeters x y
+let inches (x: float) (y: float) : Point2D<Meters, 'Coordinates> = fromUnit Length.inches x y
+let feet (x: float) (y: float) : Point2D<Meters, 'Coordinates> = fromUnit Length.feet x y
+
+
+// ---- Operators ----
 
 /// This function is designed to be used in piping operators.
 let plus (rhs: Vector2D<'Unit, 'Coordinates>) (lhs: Point2D<'Unit, 'Coordinates>) : Point2D<'Unit, 'Coordinates> =
@@ -26,7 +34,7 @@ let plus (rhs: Vector2D<'Unit, 'Coordinates>) (lhs: Point2D<'Unit, 'Coordinates>
 
 let minus (rhs: Point2D<'Unit, 'Coordinates>) (lhs: Point2D<'Unit, 'Coordinates>) : Vector2D<'Unit, 'Coordinates> =
     lhs - rhs
-    
+
 /// Be careful with the vector arguments. This function is written with piping in mind. The first point is the
 /// target location. The second point is the starting location.
 /// This is also an alias for `minus` and is `target - from`
@@ -35,7 +43,7 @@ let vectorTo
     (from: Point2D<'Unit, 'Coordinates>)
     : Vector2D<'Unit, 'Coordinates> =
     target - from
-    
+
 let neg (v: Point2D<'Unit, 'Coordinates>) : Point2D<'Unit, 'Coordinates> = -v
 
 let times (rhs: float) (lhs: Point2D<'Unit, 'Coordinates>) : Point2D<'Unit, 'Coordinates> = lhs * rhs
@@ -60,7 +68,7 @@ let toVector (point: Point2D<'Unit, 'Coordinates>) : Vector2D<'Unit, 'Coordinate
 /// Scale a point to a given length.
 let scaleTo (length: Length<'Unit>) (point: Point2D<'Unit, 'Coordinates>) : Point2D<'Unit, 'Coordinates> =
     scaleBy (length / magnitude point) point
-    
+
 /// Rotate a point counterclockwise by a given angle.
 let rotateBy (a: Angle) (p: Point2D<'Unit, 'Coordinates>) : Point2D<'Unit, 'Coordinates> =
     xy (Angle.cos a * p.X - Angle.sin a * p.Y) (Angle.sin a * p.X + Angle.cos a * p.Y)
@@ -96,7 +104,7 @@ let distanceTo p1 p2 : Length<'Unit> = distanceSquaredTo p1 p2 |> Length.sqrt
 
 let midpoint (p1: Point2D<'Unit, 'Coordinates>) (p2: Point2D<'Unit, 'Coordinates>) : Point2D<'Unit, 'Coordinates> =
     xy ((p1.X + p2.X) / 2.) ((p1.Y + p2.Y) / 2.)
-    
+
 /// Get the direction the a point is facing.
 let direction (point: Point2D<'Unit, 'Coordinates>) : Direction2D<'Coordinates> option =
     Direction2D.xyLength point.X point.Y
