767e8a158b7a0f5482b99bb2438749f04bdd4b70
Thomas Waters <twaters117@gmail.com>
2022-04-19 10:56:33 -0400

    Triangle2D unit tests

--- GeometryTests/Triangle2D.fs
+++ GeometryTests/Triangle2D.fs
@@ -9,3 +9,13 @@ open Geometry
 [<SetUp>]
 let Setup () = Gen.ArbGeometry.Register()
 
+
+[<Property>]
+let ``non-zero area triangle contains its own centroid`` (triangle: Triangle2D<Meters, TestSpace>) =
+    let centroid = Triangle2D.centroid triangle
+    let area = Triangle2D.area triangle
+
+    Test.isTrue
+        "non-zero area triangle did not contain its own centroid"
+        (area = Length.zero
+         || Triangle2D.contains centroid triangle)
