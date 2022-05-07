7f2b6cba29065acc58ac7cf028dabee4a5539104
Thomas Waters <twaters117@gmail.com>
2022-05-06 20:33:39 -0400

    Headers for 2D objects

---
+++ docs/polyline.fsx
@@ -0,0 +1,14 @@
+(**
+
+---
+title: 2D Poly-Line
+---
+
+
+*)
+(*** hide ***)
+
+#r "../Geometry/bin/Debug/net6.0/Geometry.dll"
+open Geometry
+type Cartesian = Cartesian
+(***)
---
+++ docs/triangle.fsx
@@ -0,0 +1,14 @@
+(**
+
+---
+title: 2D Triangle
+---
+
+
+*)
+(*** hide ***)
+
+#r "../Geometry/bin/Debug/net6.0/Geometry.dll"
+open Geometry
+type Cartesian = Cartesian
+(***)
