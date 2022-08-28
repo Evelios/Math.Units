8a9c3edd5d29b1a2a46e3506257e254b78a36e44
Thomas Waters <twaters117@gmail.com>
2022-08-28 09:33:57 -0400

    Changed directory naming structure

---
+++ Docs/interval.fsx
@@ -0,0 +1,22 @@
+(**
+---
+title: Interval
+category: Modules
+categoryindex: 2
+index: 3
+---
+*)
+(*** hide ***)
+
+#r "../Geometry/bin/Debug/net6.0/Geometry.dll"
+#r "../Geometry/bin/Release/net6.0/Geometry.dll"
+
+#r "../Units/bin/Debug/net6.0/Units.dll"
+#r "../Units/bin/Release/net6.0/Units.dll"
+
+open Geometry
+open Math.Units
+
+
+type Cartesian = Cartesian
+(***)
